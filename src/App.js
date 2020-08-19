ee6f88d4d97343f1114ed3b1bcd19228332689c5
Apoorva <apps3660.com>
2020-08-19 20:01:31 +0530

    google-map-API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { GoogleMap ,withScriptjs, withGoogleMap , Marker, InfoWindow } from 'react-google-maps';
+import * as parksData from "./data/skateboard-parks.json"
 
-function App() {
+
+function Map() {
+
+  const [selectedPark , setSelectedPark] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <GoogleMap  defaultZoom={10} 
+                  defaultCenter = {{ lat:45.280622216516925 , lng:-75.760235255689508 }} >
+          {parksData.features.map( (park) => (
+            <Marker  
+                key={park.properties.PARK_ID}  
+                position = {{ lat: park.geometry.coordinates[1] , 
+                              lng: park.geometry.coordinates[0] }} 
+                onClick = { () => {
+                  setSelectedPark(park);
+                }} />
+          ))}
+
+          {selectedPark && (
+            <InfoWindow  position = {{ lat: selectedPark.geometry.coordinates[1] , 
+                                       lng: selectedPark.geometry.coordinates[0] }} >
+
+              <div>Park Details</div>                            
+           </InfoWindow>
+          )}  
+
+      </GoogleMap> 
+    );
 }
 
-export default App;
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default function App(){
+    return (
+      <div  style = {{ width: "100vw" , height: "100vh" }}  >
+        <WrappedMap 
+          googleMapURL ={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDG5HkQKzfzcdYq0GvInCIKye93ygeJYkc`}
+          loadingElement = { <div style= {{ height : "100%" }} /> } 
+          containerElement = { <div style= {{ height : "100%" }} /> }
+          mapElement = { <div style= {{ height : "100%" }} /> } />
+      </div>
+    );
+};
